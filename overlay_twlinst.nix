3821774b6a49c50412d842c6ae5d7f59f55719c3
Tom <twitchyliquid64@ciphersink.net>
2022-03-12 16:09:28 -0800

    Update twlinst

--- overlay_twlinst.nix
+++ overlay_twlinst.nix
@@ -2,13 +2,13 @@
 
 super.buildGoModule rec {
   name = "twlinst";
-  version = "d588f9c7e386c23d76848730cd0d3bd4b2f80e85";
+  version = "3845b255b672917dc7edc70d3948df3d5446f184";
 
   src = super.fetchFromGitHub {
     owner = "twitchylinux";
     repo = "twlinst";
     rev = "${version}";
-    sha256 = "07arq62hv62vifdpiq6yx5yxjkzmfys16m15cbgvxzlimzm05dp2"; # use nix-prefetch-git
+    sha256 = "1d7i8698bn12886w42zc4ah8illwh6kd23nnlmz5i5izna6yivwx"; # use nix-prefetch-git
   };
 
   vendorSha256 = "sha256-FKB3YiM/zkkW5olfnaCw4AYI7YvcpvLyLSP6xHMd5mY=";

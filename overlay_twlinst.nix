4674b5a583ecede94499963ee122eae53d91d61e
Tom <twitchyliquid64@ciphersink.net>
2022-03-12 19:07:42 -0800

    Update twlinst to version with nixos-hardware support

--- overlay_twlinst.nix
+++ overlay_twlinst.nix
@@ -2,13 +2,13 @@
 
 super.buildGoModule rec {
   name = "twlinst";
-  version = "3845b255b672917dc7edc70d3948df3d5446f184";
+  version = "3d1d4e2b19e14df2ee855c4b6c391e6fe381eb2c";
 
   src = super.fetchFromGitHub {
     owner = "twitchylinux";
     repo = "twlinst";
     rev = "${version}";
-    sha256 = "1d7i8698bn12886w42zc4ah8illwh6kd23nnlmz5i5izna6yivwx"; # use nix-prefetch-git
+    sha256 = "0i9wj61cwz1ln8l7p44dw0nilv0kk9q4081k306rkgah4anrgdxa"; # use nix-prefetch-git
   };
 
   vendorSha256 = "sha256-FKB3YiM/zkkW5olfnaCw4AYI7YvcpvLyLSP6xHMd5mY=";
